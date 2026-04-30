65b3dc75cf53f7da9751c027484c2ea8ab1500b8
KittenLord <vanya.braslavets@gmail.com>
2026-04-30 16:47:03 +0300

    init

---
+++ Core/Inc/driver_display.h
@@ -0,0 +1,112 @@
+/*
+ * driver_display.c
+ *
+ *  Created on: Apr 29, 2026
+ *      Author: vanya
+ */
+
+
+#ifndef __DRIVER_DISPLAY
+#define __DRIVER_DISPLAY
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "stm32f4xx_hal.h"
+
+#define DISPLAY_PIN_RS (1 << 0)
+#define DISPLAY_PIN_RW (1 << 1)
+#define DISPLAY_PIN_EN (1 << 2)
+#define DISPLAY_PIN_BL (1 << 3)
+#define DISPLAY_PIN_D4 (1 << 4)
+#define DISPLAY_PIN_D5 (1 << 5)
+#define DISPLAY_PIN_D6 (1 << 6)
+#define DISPLAY_PIN_D7 (1 << 7)
+
+#define DISPLAY_ADDRESS (0x27 << 1)
+
+#define PINS_FROM_NIBBLE(nibble) ( \
+	(DISPLAY_PIN_D7 * (((nibble) & 0b1000) != 0)) | \
+	(DISPLAY_PIN_D6 * (((nibble) & 0b0100) != 0)) | \
+	(DISPLAY_PIN_D5 * (((nibble) & 0b0010) != 0)) | \
+	(DISPLAY_PIN_D4 * (((nibble) & 0b0001) != 0)) )
+
+#define NIBBLE_LO(byte) ((byte) & 0x0f)
+#define NIBBLE_HI(byte) (((byte) & 0xf0) >> 4)
+
+#define DISPLAY_MODE_WRITE 0
+#define DISPLAY_MODE_READ  1
+
+#define DISPLAY_REG_INSTRUCTION 0
+#define DISPLAY_REG_DATA        1
+
+#define DISPLAY_UNPACK_STATE(d) (((d).mode * DISPLAY_PIN_RW) | ((d).reg * DISPLAY_PIN_RS) | ((d).backlight * DISPLAY_PIN_BL))
+
+#define DISPLAY_LINE_SINGLE_MIN 0x00
+#define DISPLAY_LINE_SINGLE_MAX 0x4f
+
+#define DISPLAY_LINE_0_MIN 0x00
+#define DISPLAY_LINE_0_MAX 0x27
+#define DISPLAY_LINE_1_MIN_0x40
+#define DISPLAY_LINE_1_MAX 0x67
+
+#define DISPLAY_VISIBLE_LINE_LEN 16
+
+#define DISPLAY_LINE_SINGLE_LEN (DISPLAY_LINE_SINGLE_MAX - DISPLAY_LINE_SINGLE_MIN + 1)
+#define DISPLAY_LINE_LEN        (DISPLAY_LINE_0_MAX      - DISPLAY_LINE_0_MIN      + 1)
+
+typedef struct {
+	bool twoLinesInsteadOfOne;
+	bool tallFont;
+
+	bool displayOn;
+	bool cursorVisible;
+	bool cursorBlinking;
+
+	bool incrementInsteadOfDecrement;
+	bool shiftOnEntry;
+} DisplayConfig;
+
+typedef struct {
+	DisplayConfig config;
+
+	I2C_HandleTypeDef *handle;
+
+	uint8_t mode;
+	uint8_t reg;
+	bool backlight;
+} Display;
+
+void display_sendRaw(I2C_HandleTypeDef *handle, uint8_t *data, size_t len);
+
+void display_sendNibble(Display *display, uint8_t byte);
+
+void display_sync(Display *display);
+
+void display_send(Display *display, uint8_t byte);
+
+void display_writeInstruction(Display *display, uint8_t instruction);
+
+void display_writeData(Display *display, uint8_t data);
+
+void display_init(Display *display);
+
+
+
+
+void display_instruction_clearDisplay(Display *display);
+
+void display_instruction_returnHome(Display *display);
+
+void display_instruction_entryModeSet(Display *display, bool incrementInsteadOfDecrement, bool shiftOnCharacters);
+
+void display_instruction_displayOnOffControl(Display *display, bool on, bool cursorVisible, bool blinking);
+
+void display_instruction_cursorOrDisplayShift(Display *display, bool displayInsteadOfCursor, bool rightInsteadOfLeft);
+
+void display_instruction_functionSet(Display *display, bool use8bitInsteadOf4bit, bool twoLinesInsteadOfOneLine, bool tallFont);
+
+void display_instruction_setCharacterRamAddress(Display *display, uint8_t address);
+
+void display_instruction_setDisplayRamAddress(Display *display, uint8_t address);
+
+#endif // __DRIVER_DISPLAY
---
+++ Core/Src/driver_display.c
@@ -0,0 +1,144 @@
+/*
+ * driver_display.c
+ *
+ *  Created on: Apr 29, 2026
+ *      Author: vanya
+ */
+
+#include <string.h>
+#include "driver_display.h"
+
+void display_sendRaw(I2C_HandleTypeDef *handle, uint8_t *data, size_t len) {
+	HAL_I2C_Master_Transmit(handle, DISPLAY_ADDRESS, data, len, 100);
+}
+
+void display_sendNibble(Display *display, uint8_t byte) {
+	uint8_t data[2] = {0};
+	data[0] = byte | ( DISPLAY_PIN_EN);
+	data[1] = byte & (~DISPLAY_PIN_EN);
+
+	display_sendRaw(display->handle, data, sizeof(data));
+	HAL_Delay(1);
+}
+
+void display_sync(Display *display) {
+	uint8_t data = DISPLAY_UNPACK_STATE(*display);
+	display_sendRaw(display->handle, &data, 1);
+}
+
+void display_send(Display *display, uint8_t byte) {
+	uint8_t data[4] = {0};
+	data[0] = PINS_FROM_NIBBLE(NIBBLE_HI(byte)) | DISPLAY_UNPACK_STATE(*display) | DISPLAY_PIN_EN;
+	data[1] = data[0] & (~DISPLAY_PIN_EN);
+	data[2] = PINS_FROM_NIBBLE(NIBBLE_LO(byte)) | DISPLAY_UNPACK_STATE(*display) | DISPLAY_PIN_EN;
+	data[3] = data[2] & (~DISPLAY_PIN_EN);
+
+	display_sendRaw(display->handle, &data[0], 2);
+	HAL_Delay(1);
+	display_sendRaw(display->handle, &data[2], 2);
+	HAL_Delay(1);
+}
+
+void display_writeInstruction(Display *display, uint8_t instruction) {
+	Display copy = *display;
+	display->mode = DISPLAY_MODE_WRITE;
+	display->reg = DISPLAY_REG_INSTRUCTION;
+	display_send(display, instruction);
+	*display = copy;
+}
+
+void display_writeData(Display *display, uint8_t data) {
+	Display copy = *display;
+	display->mode = DISPLAY_MODE_WRITE;
+	display->reg = DISPLAY_REG_DATA;
+	display_send(display, data);
+	*display = copy;
+}
+
+
+void display_init(Display *display) {
+	uint32_t epsilon = 2;
+
+	HAL_Delay(15 + epsilon);
+	display_sendNibble(display, (DISPLAY_PIN_D5 | DISPLAY_PIN_D4));
+
+	HAL_Delay(4 + epsilon);
+	display_sendNibble(display, (DISPLAY_PIN_D5 | DISPLAY_PIN_D4));
+
+	HAL_Delay(1 + epsilon);
+	display_sendNibble(display, (DISPLAY_PIN_D5 | DISPLAY_PIN_D4));
+
+	display_sendNibble(display, (DISPLAY_PIN_D5));
+
+
+	display_instruction_functionSet(display, false, display->config.twoLinesInsteadOfOne, display->config.tallFont);
+
+	display_instruction_displayOnOffControl(display, display->config.displayOn, display->config.cursorVisible, display->config.cursorBlinking);
+
+	display_instruction_clearDisplay(display);
+
+	display_instruction_entryModeSet(display, display->config.incrementInsteadOfDecrement, display->config.shiftOnEntry);
+}
+
+
+
+
+void display_instruction_clearDisplay(Display *display) {
+	display_writeInstruction(display, 0b00000001);
+}
+
+void display_instruction_returnHome(Display *display) {
+	display_writeInstruction(display, 0b00000010);
+}
+
+void display_instruction_entryModeSet(Display *display, bool incrementInsteadOfDecrement, bool shiftOnCharacters) {
+	display_writeInstruction(display,
+		   0b00000100
+		| (0b00000010 * incrementInsteadOfDecrement)
+		| (0b00000001 * shiftOnCharacters));
+}
+
+void display_instruction_displayOnOffControl(Display *display, bool on, bool cursorVisible, bool blinking) {
+	display_writeInstruction(display,
+		   0b00001000
+		| (0b00000100 * on)
+		| (0b00000010 * cursorVisible)
+		| (0b00000001 * blinking));
+}
+
+void display_instruction_cursorOrDisplayShift(Display *display, bool displayInsteadOfCursor, bool rightInsteadOfLeft) {
+	display_writeInstruction(display,
+		   0b00010000
+		| (0b00001000 * displayInsteadOfCursor)
+		| (0b00000100 * rightInsteadOfLeft));
+}
+
+void display_instruction_functionSet(Display *display, bool use8bitInsteadOf4bit, bool twoLinesInsteadOfOneLine, bool tallFont) {
+	if(twoLinesInsteadOfOneLine) { tallFont = false; }
+	display_writeInstruction(display,
+		   0b00100000
+		| (0b00010000 * use8bitInsteadOf4bit)
+		| (0b00001000 * twoLinesInsteadOfOneLine)
+		| (0b00000100 * tallFont));
+}
+
+void display_instruction_setCharacterRamAddress(Display *display, uint8_t address) {
+	address &= 0b00111111;
+	display_writeInstruction(display, 0b01000000 | address);
+}
+
+void display_instruction_setDisplayRamAddress(Display *display, uint8_t address) {
+	address &= 0b01111111;
+	display_writeInstruction(display, 0b10000000 | address);
+}
+
+
+
+
+
+
+//void display_command_writeChar(Display *display, char c) {
+//	display_writeData(display, c);
+//}
+
+
